b9c25a2f59b0c773449d302e2ca620511850665a
Kevin MacDonald <macdkw@Kevins-MBP.home>
2022-07-25 12:18:14 -0500

    progress

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,8 +1,6 @@
-# DELIVERABLE 1
-
-## Steps 1 - 7
-SELECT 
-    e.emp_no, e.first_name, e.last_name,
+-- DELIVERABLE 1
+---- Steps 1 - 7
+SELECT e.emp_no, e.first_name, e.last_name,
     t.title, t.from_date, t.to_date
 INTO retirement_titles FROM employees AS e
 INNER JOIN titles AS t
@@ -12,19 +10,15 @@ ORDER BY e.emp_no;
 
 SELECT * FROM retirement_titles
 
-## Steps 8 - 15
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (emp_no) emp_no, 
-first_name, 
-last_name, 
-title
+---- Steps 8 - 15
+SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
 INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no ASC, to_date DESC;
 
 SELECT * FROM unique_titles;
 
-## Steps 16 - 22
+---- Steps 16 - 22
 SELECT COUNT(title), title
 INTO retiring_titles
 FROM unique_titles
@@ -33,9 +27,8 @@ ORDER BY count DESC;
 
 SELECT * FROM retiring_titles;
 
-# DELIVERABLE 2
-
-## Steps 1 - 11
+-- DELIVERABLE 2
+---- Steps 1 - 11
 SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, 
     e.birth_date, d.from_date, d.to_date, t.title
 INTO mentorship_eligibility FROM employees as e
@@ -47,5 +40,5 @@ ORDER BY e.emp_no;
 
 SELECT * FROM mentorship_eligibility;
 
-# Deliverable 3, see README.md
+-- Deliverable 3, see README.md
 
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Pewlett-Hackard-Analysis
-SQL, MySQL, PostgreSQL, pgAdmin 4
+
+The objective of this challenge is for learners to create additional two intermediate tables and a final table that contains the total number of employees by title that will be retiring based on their birth date. they'll also create a table that of employees who are still employed and born withing a certain year that are eligible to participate in a mentorship program. Finally, the learners will write a report on the results of the analysis.
+
+## Overview
+
+## Results
+Provide a bulleted list with four major points from the retirement titles and the mentorship eligibility tables. Use images if necessary to support your evidence.
+
+## Summary
+Provide a high-level summary that addresses the following questions, and then provide two additional queries or tables that can be created that will provide more insight for the upcoming "silver tsunami".
+* How many roles will need to be filled as the "silver tsunami" begins to make an impact?
+* Are there enough retirement-ready employees in the departments that are qualified to mentor the next generation of Pewlett Hackard employees?
